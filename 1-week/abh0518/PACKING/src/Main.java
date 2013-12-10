4cedbd9159dd0cdab42e829bbd264fedb0e4cf9a
abh0518 <abh0518@gmail.com>
2013-12-10 23:24:34 +0900

    PACKING 문제 풀이 시도 1. 문제 예제 정답은 처리됨 2. 알고스팟에 올리면 시간초과 나옴 3. 캐싱 처리 문제인지 알고리즘에서 배열 복사가 시간을 많이 잡아먹는지 확인 필요

---
+++ 1-week/abh0518/PACKING/src/Main.java
@@ -0,0 +1,117 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private String[] luggage;
+	private int size[];
+	private int need[];
+	private Map<String, boolean[]> cache = new HashMap<String, boolean[]>();
+	
+	public Main(String[] luggage, int size[], int need[]){
+		this.luggage = luggage;
+		this.size = size;
+		this.need = need;
+	}
+	
+	public boolean[] pack(int backSize){
+		boolean[] selectList = new boolean[luggage.length];
+		Arrays.fill(selectList, false);
+		boolean[] luggageList = new boolean[luggage.length];  
+		Arrays.fill(luggageList, true);
+		return pack(selectList, luggageList, backSize);
+	}
+	
+	public String getKey(boolean[] luggageList, int backSize){
+		return Arrays.toString(luggageList)+backSize;
+	}
+	
+	public boolean[] pack(boolean[] selectList, boolean[] luggageList, int backSize){
+		String key = getKey(luggageList, backSize);
+		if(cache.get(key) != null) return cache.get(key);
+		
+		boolean[] result = Arrays.copyOf(selectList, selectList.length);
+		
+		for(int i = 0 ; i < luggage.length ; i++){
+			if( backSize-size[i] >= 0 && luggageList[i] == true){
+				boolean[] nSelectList = Arrays.copyOf(selectList, selectList.length);
+				boolean[] nLuggageList = Arrays.copyOf(luggageList, luggageList.length);
+				
+				nSelectList[i] = true;
+				nLuggageList[i] = false;
+				
+				nSelectList = pack(nSelectList, nLuggageList, backSize-size[i]);
+				result = getMaxSelect(result, nSelectList);
+			}
+		}
+		
+		cache.put(key, result);
+		return result;
+	}
+	
+	private boolean[] getMaxSelect(boolean[] aSelect, boolean[] bSelect){
+		
+		int sumA = 0;
+		for(int i = 0 ; i < aSelect.length ; i++){
+			if(aSelect[i] == true) sumA += need[i];
+		}
+		
+		int sumB = 0;
+		for(int i = 0 ; i < bSelect.length ; i++){
+			if(bSelect[i] == true) sumB += need[i];
+		}
+		
+		return sumA > sumB ? aSelect : bSelect; 
+	}
+	
+	
+	
+	public static void main(String args[]) throws FileNotFoundException{
+		
+		//Scanner in = new Scanner(new File("test.txt"));
+		Scanner in = new Scanner(System.in);
+		
+		int testCount = in.nextInt();
+		
+		for(int i = 0 ; i < testCount ; i++){
+			int luggageCount = in.nextInt();
+			int backSize = in.nextInt();
+			
+			String[] luggageName = new String[luggageCount];
+			int[] size = new int[luggageCount];
+			int[] need = new int[luggageCount];
+			in.nextLine();
+			for(int j = 0 ; j < luggageCount ; j++){
+				String str = in.nextLine();
+				StringTokenizer tk = new StringTokenizer(str," ");
+				luggageName[j] = tk.nextToken();
+				size[j] = Integer.parseInt(tk.nextToken());
+				need[j] = Integer.parseInt(tk.nextToken());
+				
+			}
+			
+			Main main = new Main(luggageName,size,need);
+			boolean selectList[] = main.pack(backSize);
+			
+			int needSum = 0;
+			int packCount = 0;
+			for(int j = 0 ; j < selectList.length ; j++){
+				if(selectList[j] == true) {
+					needSum += need[j];
+					packCount++;
+				}
+			}
+			System.out.println(needSum + " " + packCount);
+			
+			for(int j = 0 ; j < selectList.length ; j++){
+				if(selectList[j] == true) System.out.println(luggageName[j]);
+			}
+	
+		}		
+	}
+
+}
